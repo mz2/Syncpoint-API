c637dee9c884e851c9125f9ff037b020ebbe96d3
Chris Anderson <jchris@apache.org>
2012-02-27 16:50:05 -0800

    basic dependency injection for tests, still not completely passing, will come back to it with a fresh brain

--- test/pairing.js
+++ test/pairing.js
@@ -1,3 +1,14 @@
+var sessionFromFacebook = require('../lib/sessionFromFacebook');
+sessionFromFacebook._dependencies.getMeFromFB = function(code, cb) {
+    console.log('getMeFromFB stubbed');
+    assert.equal(code, "stubbed-token")
+    var fixtureResponse = {
+        name : "Mr. Miyagi",
+        id : 123456789
+    };
+    cb(false, fixtureResponse);
+};
+
 var SyncpointAPI = require('../lib/syncpoint-api'),
     coux = require('coux').coux,
     assert = require("assert")
@@ -16,81 +27,52 @@ var testConfig = {
     }
 }
 
-
-
 function smallRand() {
     return Math.random().toString().substr(2,4);
 };
 
-var db = "http://localhost:5984/test-control";
+var db = testConfig.apps['test-app'].host + '/' + testConfig.apps['test-app']["handshake-db"];
 // setup the database
 coux.del(db, function() {
     coux.put(db, e(function() {
-        // launch the controller engine
-        controller.start({control:db, server : "http://localhost:5984"});
-        console.log("started controller")
-        // save doc for new device
-        coux.post(db, {
-            // _id : device_id,
-            owner : "email@example.com",
-            type : "device",
-            state : "new",
-            device_code : smallRand(),
+        // launch the Syncpoint API engine
+        var syncpoint = new SyncpointAPI(testConfig);
+        syncpoint.start();
+        console.log("started API listeners")
+        // save hanshake doc for new session
+        var handshakeDoc = {
             oauth_creds : { // we need better entropy
               consumer_key: smallRand(),
               consumer_secret: smallRand(),
               token_secret: smallRand(),
               token: smallRand()
-            }
-        }, e(function(err, ok) {
-            console.log("created new device", ok.id)
-            coux.post({url:db+"/_changes?filter=_doc_ids&since=1&include_docs=true&feed=longpoll", agent:false}, {"doc_ids": [ok.id]}, e(function(err, resp) {
+            },
+           "type": "session-fb",
+           "fb_access_token": "stubbed-token",
+           "state": "new"
+        };
+        
+
+        coux.post(db, handshakeDoc, e(function(err, ok) {
+            console.log("created new handshake document", ok.id, "listening for changes on it");
+            coux.log == ["post"];
+            coux.post({url:db+"/_changes?filter=_doc_ids&since=1&include_docs=true&feed=longpoll", agent:false}, 
+                {"doc_ids": [ok.id]}, e(function(err, resp) {
+                    console.log(resp)
                 var doc = resp.results[0].doc;
-                assert.equal(doc.state, "confirming")
-                console.log("confirming device")
-                // create a doc like we clicked confirm
-                coux.post(db, {
-                    type : "confirm",
-                    state : "clicked",
-                    device_code : doc.device_code, 
-                    confirm_code : doc.confirm_code
-                }, e(function(err, ok) {
-                    console.log("confirmed device")
-                    coux.post({url:db+"/_changes?filter=_doc_ids&since=3&include_docs=true&feed=longpoll", agent:false}, {"doc_ids": [ok.id]}, e(function(err, resp) {
-                        var conf = resp.results[0].doc;
-                        assert.equal(conf.state, "used")
-                        console.log("confirm used")
-                        coux.post({url:db+"/_changes?filter=_doc_ids&since=5&include_docs=true&feed=longpoll", agent:false}, {"doc_ids": [doc._id]}, e(function(err, resp) {
-                            var doc = resp.results[0].doc;
-                            console.log("device active")
-                            assert.equal(doc.state, "active")
-                            // check that the oauth creds work for replication...
-                                // check the users database for the document for this user
-                                var server = db.split('/');
-                                server.pop(); server = server.join('/');
-                                coux([server, '_users', "org.couchdb.user:"+doc.owner], e(function(err, info) {
-                                    console.log(info)
-                                    console.log('test complete')
-                                    process.exit()
-                                }))
-                                // create a private database and document for the user
-                                // check to see that we can read it into a public 
-                                // database via replication as the user
-                        }));
-                    }));     
+                assert.equal(doc.state, "active")
+                console.log("active handshake")
+                // check that the oauth creds work for replication...
+                // check the users database for the document for this user
+                var server = testConfig.apps['test-app'].host;
+                coux([server, '_users', "org.couchdb.user:"+doc.session.user_id], e(function(err, info) {
+                    console.log(info)
+                    console.log('test complete')
+                    process.exit()
                 }))
             }))
         }));
     }))
 });
 
-
-
-// stub sending an email
-
-// write validation doc
-
-// ensure device can connect as user
-
-// find patches that we need
 
