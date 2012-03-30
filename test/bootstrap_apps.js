df5f64689db476f9aced7b9e3a916c837589a3dd
Chris Anderson <jchris@apache.org>
2012-03-30 15:06:50 -0700

    starting move of facebook test to tap

--- test/bootstrap_apps.js
+++ test/bootstrap_apps.js
@@ -1,42 +1,45 @@
 var testConfig = require('./testConfig');
 
-var coux = require('coux').coux
+var tap = require("tap")
+    , coux = require('coux').coux
+    , test = tap.test
+    , plan = tap.plan
     , SyncpointAPI = require('../lib/syncpoint-api')
     ;
 
-exports.bootstrap = {
-  setUp : function(done) {
-    // delete test databases so we can ensure they are created
-    var syncpoint = new SyncpointAPI(testConfig);
-    coux.del([testConfig.host, testConfig.config_db], function() {
-        coux.del([testConfig.host, testConfig.handshake_db], function() {
-          syncpoint.start(done);
-        })
-    })
-  },
-  'create the config database': function(t) {
-    coux([testConfig.host, testConfig.config_db], function(err, ok) {
-      t.expect(1)
-        t.ok(ok.db_name, "config db exists")
-        t.done()
-    })
-  },
-  "create the config db design doc" : function(t) {
-    coux([testConfig.host, testConfig.config_db, "_design/config"], function(err, doc) {
-        t.ok(doc.views, "config ddoc exists")
-        t.done()
-    })
-  },
-  "create the users_db design doc" : function(t) {
-    coux([testConfig.host, testConfig.users_db, "_design/syncpoint"], function(err, doc) {
-        t.ok(doc.views, "users ddoc exists")
-        t.done()
-    })
-  },
-  'create the handshake database' : function(t) {
-    coux([testConfig.host, testConfig.handshake_db], function(err, ok) {
-        t.ok(ok.db_name, "handshake db exists")
-        t.done()
-    })
-  }
-}
+var syncpoint = new SyncpointAPI(testConfig);
+
+setTimeout(function() {
+  console.log("timeout");
+  process.exit()
+},2 * 1000);
+
+// delete test databases so we can ensure they are created
+console.log("setup")
+coux.del([testConfig.host, testConfig.config_db], function() {
+    coux.del([testConfig.host, testConfig.handshake_db], function() {
+      console.log("start")
+        syncpoint.start(function(err) {
+            test('create the config database', function(t) {
+                coux([testConfig.host, testConfig.config_db], function(err, ok) {
+                    t.ok(ok.db_name, "config db exists")
+                    t.end()
+                })
+            })
+            test("create the config db design doc", function(t) {
+                coux([testConfig.host, testConfig.config_db, "_design/config"], function(err, doc) {
+                    t.ok(doc.views, "config ddoc exists")
+                    t.end()
+                })
+            })
+            test('create the handshake database', function(t) {
+                coux([testConfig.host, testConfig.handshake_db], function(err, ok) {
+                    t.ok(ok.db_name, "handshake db exists")
+                    t.end()
+                })
+            })
+        });
+    });
+});
+
+
---
+++ test/testFacebookPairing.js
@@ -0,0 +1,120 @@
+var sessionFromFacebook = require('../lib/sessionFromFacebook');
+sessionFromFacebook._dependencies.getMeFromFB = function(code, cb) {
+    console.log('getMeFromFB stubbed');
+    assert.equal(code, "stubbed-token")
+    var fixtureResponse = {
+        name : "Mr. Miyagi",
+        id : 123456789
+    };
+    cb(false, fixtureResponse);
+};
+
+var SyncpointAPI = require('../lib/syncpoint-api'),
+    coux = require('coux').coux,
+    assert = require("assert"),
+    docstate = require("docstate"),
+    tap = require("tap"), test = tap.test, plan = tap.plan,
+    e = require('errLog').e;
+
+// tests should also be capable of testing other implementations
+// eg: integration tests
+
+var testConfig = require('./testConfig');
+
+
+function smallRand() {
+    return Math.random().toString().substr(2,4);
+}
+
+var handshake_db = testConfig.host + '/' + testConfig.handshake_db;
+// setup the database
+var server = testConfig.host;
+
+// exports.pairing = {
+//   setUp : function(done) {
+//     
+//   }
+// }
+// return;
+// describe("pairing with facebook", function() {
+//     var handshakeId, handshakeDoc, userControlDb;
+//     
+// exports["pairing with facebook"] = {
+//   "should create the handshake db on the server": function(t) {
+//     t.done()
+//   }
+// }
+
+
+    // 
+    //     it("should create the handshake db on the server", function() {
+    //         coux.del(handshake_db, function() {
+    //             var syncpoint = new SyncpointAPI(testConfig);
+    //             syncpoint.start(function(err) {
+    //                 console.log("syncpoint started")
+    //                 coux(handshake_db, function(err, info) {
+    //                     console.log("handshake db info", info)
+    //                     expect(err).toEqual(false)
+    //                     expect(info.db_name).toEqual(testConfig.handshake_db)
+    //                     test.done()
+    //                 })
+    //             });
+    //         });
+    //         asyncSpecWait()
+    //     });
+    // 
+    //     it("and a handshake doc", function() {
+    //         console.log("testing handshake");
+    //         var handshakeDoc = {
+    //             oauth_creds : {
+    //               consumer_key: smallRand(),
+    //               consumer_secret: smallRand(),
+    //               token_secret: smallRand(),
+    //               token: smallRand()
+    //             },
+    //            "type": "session-fb",
+    //            "fb_access_token": "stubbed-token",
+    //            "state": "new"
+    //         };
+    //         coux.post(handshake_db, handshakeDoc, function(err, ok) {
+    //             expect(err).toEqual(false)
+    //             console.log("did handshake", ok);
+    //             handshakeId = ok.id;
+    //             test.done()
+    //         })
+    //         asyncSpecWait()
+    //     })
+    //     it("when the doc is active", function(test) {
+    //         testHelper.waitForDoc(handshake_db, handshakeId, 1, function(err, doc) {
+    //             expect(err).toEqual(false)
+    //             expect(doc.state).toEqual("active")
+    //             handshakeDoc = doc
+    //             test.done()
+    //         })
+    //     })
+    //     it("should update the user doc", function(test) {
+    //         coux([server, testConfig.users_db, handshakeDoc.user_id], e(function(err, user) {
+    //             console.log("user",user.full_name)
+    //             expect(user.state).toEqual('has-control')
+    //             expect(user.oauth.consumer_keys[handshakeDoc.oauth_creds.consumer_key])
+    //                 .toEqual(handshakeDoc.oauth_creds.consumer_secret);
+    //             expect(user.control_database).toBeDefined()
+    //             userControlDb = user.control_database;
+    //             test.done()
+    //         }))
+    //     })
+    //     it("should create the control database", function(test) {
+    //         coux([server, userControlDb], function(err, ok) {
+    //             expect(err).toEqual(false)
+    //             test.done()
+    //         })
+    //     })
+    //     it("should install the control design doc", function(test) {
+    //         coux([server, userControlDb, "_design/control"], function(err, doc) {
+    //             expect(err).toEqual(false)
+    //             expect(doc.views || false).not.toEqual(false)
+    //             test.done()
+    //         })
+    //     })
+    // })
+    // 
