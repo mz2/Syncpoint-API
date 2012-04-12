4efaf32bff977fe389a65d98029cdc2d9aecb945
Chris Anderson <jchris@apache.org>
2012-04-12 10:16:44 -0700

    clean up the config process

--- test/testConfig.js
+++ test/testConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    "host" : "http://jchrisa:jchrisa@jchrisa.iriscouch.com",
+    "host" : "http://Administrator:ok@localhost:5984",
     "config_db" : "test_sp_config",
     "global_control_db": "test_sp_control",
     "users_db" : "test_sp_users"
