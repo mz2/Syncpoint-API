bd5878f81e335ffa062bba81067f0f6cd4d8dc07
Chris Anderson <jchris@apache.org>
2012-04-17 10:11:35 -0700

    test config picks up host (and admin credentials) from master config

--- test/testConfig.js
+++ test/testConfig.js
@@ -1,5 +1,20 @@
+var fs = require('fs'),  
+  path = require("path"),
+  configPath = path.join(__dirname, "..","config.json"),
+  ok = false,
+  config = fs.readFileSync(configPath, "utf8");
+
+try {
+  config = JSON.parse(config);
+  ok = true;
+} catch (e) {
+  console.error("could not read config.json, please use `npm start syncpoint` to set up your installation: "+configPath);
+}
+
+// we use the configured host but test databases.
+// if there is a test_host configured we use that instead
 module.exports = {
-    "host" : "http://Administrator:ok@localhost:5984",
+    "host" : config.test_host || config.host,
     "admin_db" : "test_sp_admin",
     "users_db" : "test_sp_users"
 }
